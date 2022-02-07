8c627e2320a9069a9366d1fe079d03311df4829e
anemadhon <anemadhon@gmail.com>
2022-02-07 10:55:59 +0700

    modify logic gate manage-project-tasks

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -102,7 +102,7 @@ class AuthServiceProvider extends ServiceProvider
             $pmProject = $user->id === $project->users->first()?->pivot->pm_id;
             $hasTeam = $project->users_count > 0;
 
-            return (($isAdmin || $isManager) || ($pmProject && $hasTeam));
+            return ($hasTeam && ($isAdmin || $isManager) || ($pmProject && $hasTeam));
         });
 
         Gate::define('manage-sub-tasks', function(User $user, Task $task)
