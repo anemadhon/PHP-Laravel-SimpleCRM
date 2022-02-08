a08f62dca5ccc309de428af5e6c006e5b8db217e
anemadhon <anemadhon@gmail.com>
2022-02-08 17:17:28 +0700

    add custom error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,33 @@
+<x-guest-layout>
+    <div class="px-4 w-full lg:w-4/12">
+        <div class="flex relative flex-col mb-6 w-full min-w-0 break-words rounded-lg border-0 shadow-lg bg-blueGray-200">
+
+            <div class="px-6 py-6 mb-0 rounded-t">
+                <div class="mb-3 text-center">
+                    <p class="font-bold text-blueGray-500" style="font-size: 6em">
+                        {{__('403')}}
+                    </p>
+                </div>
+                
+                <hr class="border-b-1 border-blueGray-300"/>
+                
+                <div class="mb-1 text-center">
+                    <p class="text-lg text-blueGray-500">
+                        {{__('This Action is Unauthorized')}}
+                    </p>
+                </div>
+
+                <hr class="border-b-1 border-blueGray-300"/>
+            </div>
+
+            <div class="flex-auto px-4 py-10 pt-0 lg:px-10">
+                <a href="{{ route('dashboard') }}">
+                    <x-button class="w-full">
+                        {{ __('Back to Dashboard') }}
+                    </x-button>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</x-guest-layout>
