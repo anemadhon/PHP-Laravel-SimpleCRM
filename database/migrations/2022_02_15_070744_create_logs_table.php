b24e5c2048c33be970b6cf30a0d276ea5c600948
anemadhon <anemadhon@gmail.com>
2022-02-16 10:14:01 +0700

    tabke schema for logs table

---
+++ database/migrations/2022_02_15_070744_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('method');
+            $table->string('action');
+            $table->string('detail');
+            $table->string('status');
+            $table->string('session_id');
+            $table->string('from_ip');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
